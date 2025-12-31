33da047b8cb7d366f8ea5b39dc578f2b3d1b8b7a
Vito Secona <secona00@gmail.com>
2026-01-01 01:00:16 +0700

    load full from zip

--- src/head.js
+++ src/head.js
@@ -1,10 +1,13 @@
 import JSZip from "jszip";
+import { load } from "npyjs";
 import { parseOBJ } from "./util.js";
 
 export class HeadData {
-  constructor(obj, texMap) {
+  constructor(obj, texMap, img, recon) {
     this.obj = obj;
     this.texMap = texMap;
+    this.img = img;
+    this.recon = recon;
   }
 
   static async load(dataFile) {
@@ -16,6 +19,30 @@ export class HeadData {
     const texMapBlob = await zip.file("HRN_export/HRN_result.jpg").async("blob");
     const texMap = URL.createObjectURL(texMapBlob);
 
-    return new HeadData(obj, texMap);
+    const imgBlob = await zip.file("HRN_export/HRN_preprocessed.jpg").async("blob");
+    const imgUrl = URL.createObjectURL(imgBlob);
+    const img = new Image();
+    img.src = imgUrl;
+    await new Promise((resolve) => { img.onload = resolve; });
+
+    const npzData = await zip.file("HRN_recon.npz").async("arraybuffer");
+    const npzZip = await JSZip.loadAsync(npzData);
+
+    const loadNpy = async (filename) => {
+      const file = npzZip.file(filename);
+      const buffer = await file.async("arraybuffer");
+      const result = await load(buffer)
+      return result.data;
+    };
+
+    const recon = {
+      trans: await loadNpy("trans.npy"),
+      angle: await loadNpy("angle.npy"),
+      cameraFocal: await loadNpy("camera_focal.npy"),
+      cameraCenter: await loadNpy("camera_center.npy"),
+      cameraDist: await loadNpy("camera_dist.npy"),
+    };
+
+    return new HeadData(obj, texMap, img, recon);
   }
 }
